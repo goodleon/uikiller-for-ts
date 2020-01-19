db91087921df16b79588e82ae852999a94a0c2cc
传祥 <wangchuanxiang@happyplaygame.net>
2020-01-19 17:51:37 +0800

    style: 使用$绑定同类型节点

--- assets/cases/uikiller/test3.ts
+++ assets/cases/uikiller/test3.ts
@@ -10,13 +10,14 @@ export default class Test3 extends Thor {
     onLoad() {
         for(let i = 1; i <= this._content.childrenCount; i++) {
             let name = '_image' + i;
+            cc.log('wcx0119 name=' + name)
             let node = this[name];
             //节点下的节点用Name就可以访问，但是Label没“_”开头，不能用$访问节点下的组件
             node.num.getComponent(cc.Label).string = i.toString();
         }
     }
 
-    _onImageTouchEnd(sender) {
+    _onImageTouchEnd(sender) { // $ 同类型, _image$1,_image$2, _image$3,
         this._logLabel.$Label.string = `你点击了${sender.$}，他的节点名字为"${sender.name}"`;
     }
 };
