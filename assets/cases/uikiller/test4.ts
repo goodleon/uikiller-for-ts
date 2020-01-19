7b2c0987c8ce3a0f86b32a96a373fe4b1070290b
传祥 <wangchuanxiang@happyplaygame.net>
2020-01-19 19:07:56 +0800

    style: 使用@符号过虑节点

--- assets/cases/uikiller/test4.ts
+++ assets/cases/uikiller/test4.ts
@@ -1,22 +1,25 @@
 import Thor from '../../uikiller/Thor';
 
 const {ccclass} = cc._decorator;
-
+// 有 @的节点就不被处理了..
 @ccclass
 export default class Test4 extends Thor {
     getOptions() {
         return {
             filter: (node) => {
-                return node.$ % 2;
+                let ival = node.$ % 2
+                cc.log(`wcx0119 ${node.$} ${ival} name= ${node.name}` )
+                return ival; // 返回0的node才符合要求
             }
         }
     }
 
     onLoad() {
         for (let i = 1; i <= 8; i++) {
+            cc.log('wcx0119 i=' + i)
             let imageNode = this[`_image${i}`];
             if (imageNode.num) {
-                imageNode.num.getComponent(cc.Label).string = i;
+                imageNode.num.getComponent(cc.Label).string = `#` + i;
             }
         }
     }
